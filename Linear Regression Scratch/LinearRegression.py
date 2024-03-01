9c48afcdacf80e217bd3cc6b764b969c21ce4067
Imran Khan <imran995353@gmail.com>
2024-03-01 22:15:47 +0530

    adding jupyter notebook explanation

--- Linear Regression Scratch/LinearRegression.py
+++ Linear Regression Scratch/LinearRegression.py
@@ -1,15 +1,22 @@
-import numpy as np
+from IPython.display import display, clear_output
 import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 class LinearRegression:
+    
     def __init__(self):
-        pass
+        self.w = 0
+        self.b = 0
+        self.alpha = 0.01
+        self.iterations = 10000
+
     def computeCost(self, x, y, w, b):
         """
         Calculate cost or squared error on the data using: (1 / (2 * m))sum((y_hat - y)^2)
         """
         m = len(y)
-        y_hat = w * x + b
+        y_hat = x @ w + b
         squared_diff = (y_hat - y)**2
         cost = (1 / (2 * m)) * sum(squared_diff)
         return cost
@@ -20,7 +27,43 @@ class LinearRegression:
         """
         m = len(y)
         # derivative w.r.t w
-        y_hat = w * x + b
-        dw = (1/m) * sum((y_hat - y) * x)
+        y_hat = x @ w + b
+        dw = (1/m) * sum((y_hat - y) @ x)
         db = (1/m) * sum(y_hat - y)
         return dw, db
+    
+    def predict(self, x, w, b):
+        """
+        Predicts y given input x
+        """
+        y_pred = x @ w + b
+        return np.ceil(y_pred)
+    
+class Dataset:
+
+    def __init__(self):
+        pass
+
+    def generate(self, x):
+        """
+        Simple quadratic function to generate numbers from single variable
+        """
+        y = x**2 + 3
+
+        return y
+    def linear_transform(self, x, w, b):
+        """
+        Perform a linear transformation on the input array x using weights/slope w and bias/intercept b.
+        Args:
+            x: Input array of independent data.
+            w: Weight vector or slope vector.
+            b: Bias value or intercept.
+        Returns:
+            Transformed matrix.
+        """
+        return x @ w + b
+    
+if __name__ == "__main__":
+    from sklearn.linear_model import LinearRegression
+
+    print("HE")
